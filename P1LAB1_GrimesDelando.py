3220719e274912b46d81435f8345d2dbf2d9b773
grimesd5475-sys <grimesd5475@student.faytechcc.edu>
2026-03-01 00:22:47 -0500

    Add files via upload

---
+++ P1LAB1_GrimesDelando.py
@@ -0,0 +1,12 @@
+# Delando Grimes
+# Date: 02/22/2026
+# Assignment: P1LAB1 - Input and Output Basics
+# Description: This program asks the user for their first and last name
+# and then displays a personalized welcome message.
+
+# Get user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("Welcome to CTI-110,", first_name, last_name + "!")
